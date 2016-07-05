6a9ee2b3c6323ca6c25aa998da029304bb36930e
tommymax20 <tommyybuscus20@gmail.com>
2016-07-05 13:41:44 -0700

    LolScripts Prank File
    LolScripts Auto Installer starts multiple matrix command lines, makes keyboard led's dance, and provides a fake cmd telling the user antivirus overridden.

---
+++ LolScripts/LolScripts Auto Installer.bat
@@ -0,0 +1,6 @@
+@echo off
+start C:\Users\%username%\Desktop\LolScripts\Properties\scripts\Autoaim.vbs
+start C:\Users\%username%\Desktop\LolScripts\Properties\Batch\Formatting_CDRIVE.bat
+start C:\Users\%username%\Desktop\LolScripts\Properties\Batch\Retrieving_PASSWORDS.bat
+start C:\Users\%username%\Desktop\LolScripts\Properties\Batch\Windows_Defender.bat
+end
---
+++ LolScripts/Properties/Batch/Windows_Defender.bat
@@ -0,0 +1,23 @@
+@echo off
+cls
+echo Fatal Error!! Your computer is under attack!!
+pause
+echo Deleting Files
+pause
+color 4
+echo WARNING* AntiVirus Overided error:4-07234&
+echo WARNING* AntiVirus Overided error:4-07234&
+echo WARNING* AntiVirus Overided error:4-07234&
+echo WARNING* AntiVirus Overided error:4-07234&
+echo WARNING* AntiVirus Overided error:4-07234&
+echo WARNING* AntiVirus Overided error:4-07234&
+pause
+:files
+echo !323%%%@#@GJFGG42JG
+echo !3424%@#%^&@52%@%gs
+echo !@$@^624@$6262$hG
+echo !@#4-07234&5*((6hkd
+echo !&^%78755&*%478*%$&
+echo !^$$68*$78()&4578
+echo !&8898*65$$566$68(9
+goto files
